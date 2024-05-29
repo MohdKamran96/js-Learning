5f676c47e0789a97ccba74b1293dce08878b5699
Mohd Kamran96 <73494399+MohdKamran96@users.noreply.github.com>
2024-05-29 16:04:36 +0000

    container config done

---
+++ JavaScript_Practice/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // Everything is Newer Means use high standard
+// Code readabilty should be high
+
+
+// Datatypes 
+let name="Kamran";   
+let age= 20;         
+let isloggedIn=true;  // Boolean
+
+
+// data Types in JavaScript
+// String   
+// Integer: range 2 the power 53 Approx
+// Boolean: true/ false
+// null is standalone value 
+// undefined 
+// symbol:  for Unique 
+// object
+
+
+console.log(typeof null) // Object
+console.log(typeof undefined) // undefined
---
+++ JavaScript_Practice/variables.js
@@ -0,0 +1,10 @@
+const account_Id=123654;
+let accountEmail="kamran@gmail.com";
+var accountPassward="26566";
+accountCity="Hyderabad";
+let accountState;
+//nodeaccount_Id=2;  // not Allowed
+/*
+Prefer to Not Use Var Because of Issues in Block Scope and Functional Scope 
+*/
+ console.table([account_Id,accountEmail,accountPassward,accountCity,accountState]);
